7cf23b7b8d8b34a38dc0bf581f6dc14e7adfa52b
Eluanatein <116521829+Eluanatein@users.noreply.github.com>
2024-07-11 10:38:51 +0100

    Delete BookreviewApi directory

--- BookreviewApi/BookreviewApi/Model/USER.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookreviewApi.Model
-{
-    public class USER
-    {
-        public int Id { get; set; }
-        public  string Username { get; set; }
-        public ICollection<RATING>? RATINGS { get; set; }
-    }
-}
